f403b301a8b5854816b8c0bf9712045c336a86b0
user <vinaygoudmaduri@gmail.com>
2026-06-25 13:28:28 +0530

    training commit

--- catalogue.tf
+++ catalogue.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-01f6d27b7cf60481b"]
 
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "catalogue" {
   zone_id = "Z0318615248O13Q4XTX"
-  name    = "frontend-dev"
+  name    = "catalogue-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip ]
+  records = [aws_instance.catalogue.private_ip ]
 }
--- mongodb.tf
+++ mongodb.tf
@@ -1,17 +1,17 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-01f6d27b7cf60481b"]
 
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = "Z0318615248O13Q4XTX"
-  name    = "frontend-dev"
+  name    = "mongodb-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip ]
+  records = [aws_instance.mongodb.private_ip ]
 }
