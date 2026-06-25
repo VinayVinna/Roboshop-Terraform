730a1c1de5dcdb53c5202fedd16e57023404e823
user <vinaygoudmaduri@gmail.com>
2026-06-25 14:06:50 +0530

    training commit

--- catalogue.tf
+++ catalogue.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "catalogue" {
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -u https://github.com/VinayVinna/Roboshop-shell.git Roboshop.yml -e component_name=catalogue -e env=dev"
+      "ansible-pull -i localhost, -u https://github.com/VinayVinna/Roboshop-ansible.git Roboshop.yml -e component_name=catalogue -e env=dev",
 
     ]
   }
