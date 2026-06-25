5675ccedf3d906ad375bc311046b69f88e1b464b
user <vinaygoudmaduri@gmail.com>
2026-06-25 13:14:17 +0530

    training commit

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-01f6d27b7cf60481b"]
 
   tags = {
     Name = "frontend"
