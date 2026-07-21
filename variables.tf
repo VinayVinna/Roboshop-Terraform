c7ad6141da51176a17df37342ad814dabbb9ca71
VinayVinna <vinaygoudmaduri@gmail.com>
2026-07-21 10:23:00 +0530

    Roboshop Code

--- variables.tf
+++ variables.tf
@@ -1,20 +1,23 @@
-variable "ami_id" {
-  default = "ami-0220d79f3f480ecf5"
-}
-
-variable "instance_type" {
-  default = "t3.small"
-}
-
 variable "vpc_security_group_ids" {
   default = ["sg-01f6d27b7cf60481b"]
 }
 
 variable "instances" {
   default = {
-    mongodb = null
-    catalogue =null
-    frontend = null
+    mongodb = {
+      ami_id = "ami-0220d79f3f480ecf5"
+      instance-type = "t3.small"
+    }
+
+    catalogue = {
+      ami_id = "ami-0220d79f3f480ecf5"
+      instance-type = "t3.small"
+    }
+
+    frontend = {
+      ami_id = "ami-0220d79f3f480ecf5"
+      instance-type = "t3.small"
+    }
   }
 }
 
